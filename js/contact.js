10a069874b5216bbcc48145015615e87ebbf2fec
tico <lours2021@gmail.com>
2021-01-27 19:40:02 +0900

    Update about css

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,9 @@
 class App {
   constructor() {
-    const background = document.getElementById("background");
     const body = document.body;
 
-    const backgroundChangeWidth = function(ms) {
-      new Promise((resolve) => {
-        setTimeout(() => {
-          background.style.width = "50%";
-        }, ms);
-      });
-    }
-
     body.addEventListener('click', function() {
-      backgroundChangeWidth(500).then(function() {
-        location.href = 'index.html';
-      });
+      location.href = 'index.html';
     });
   }
 }
